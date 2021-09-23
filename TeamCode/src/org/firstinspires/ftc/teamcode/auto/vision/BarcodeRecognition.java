335aa2fc53afe351f875f714bb8ff5214852ee76
AusreisserSF <45668270+AusreisserSF@users.noreply.github.com>
2021-09-23 14:19:17 -0700

    WIP: barcode recognition: findTeamScoringElement (9/23/21 v2)

--- TeamCode/src/org/firstinspires/ftc/teamcode/auto/vision/BarcodeRecognition.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/auto/vision/BarcodeRecognition.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.auto.vision;
 
 //!! IntelliJ only
+
 import org.firstinspires.ftc.ftcdevcommon.intellij.WorkingDirectory;
 import org.firstinspires.ftc.ftcdevcommon.intellij.TimeStamp;
 
@@ -31,21 +32,12 @@ public class BarcodeRecognition {
     }
 
     // Returns the result of image analysis.
-    
-        //**TODO Watch out for glare (see Image_04170921_15573_ROI_2021_07_14_19-18-10_214_GRAY.png)
-        // and shadows (see Image_04170921_15573_ROI_2021_07_14_19-18-10_214_SHARP.png)
-        //** Make sure the height of the custom element allows us to define an ROI that cuts off its
-        // shadow.
-        /*
-           <TE02> Size Constraints - The maximum size of the Team Scoring Element is 4 inches (10.16 cm)
-                  by 4 inches (10.16 cm) by 8 inches (20.32 cm). The minimum size of the Team Scoring
-                  Element is 3 inches (7.62 cm) by 3 inches (7.62 cm) by 4 inches (10.16 cm).
-           <TE03> Team Number - Team Scoring Elements must be labeled with their Team number (numerals
-                  only, for example “12345”) The letters must be legible when viewed from a distance of
-                  12 inches (30.48 cm) away. The Team number needs to appear only once on the Team
-                  Scoring Element.
-        */
 
+    //**TODO Watch out for glare (see Image_04170921_15573_ROI_2021_07_14_19-18-10_214_GRAY.png)
+    // Matte black may be best.
+    // and shadows (see Image_04170921_15573_ROI_2021_07_14_19-18-10_214_SHARP.png)
+    //** Make sure the height of the custom element allows us to define an ROI that cuts off its
+    // shadow.
     public BarcodeReturn findTeamScoringElement(ImageProvider pImageProvider, BarcodeParameters pBarcodeParameters) throws InterruptedException {
 
         RobotLogCommon.d(TAG, "In BarcodeRecognition.findTeamScoringElement");
@@ -88,18 +80,18 @@ public class BarcodeRecognition {
         Imgcodecs.imwrite(imageFilename, imageROI);
 
         // Adapted from ...\OpenCV_Projects\OpenCVTestbed2\OpenCVTestbed2\GeneralTarget::analyzeSkystoneStripe
-	    // We're on the grayscale path.
-	    Mat grayROI = new Mat();
-	    Imgproc.cvtColor(imageROI, grayROI, Imgproc.COLOR_BGR2GRAY);
+        // We're on the grayscale path.
+        Mat grayROI = new Mat();
+        Imgproc.cvtColor(imageROI, grayROI, Imgproc.COLOR_BGR2GRAY);
 
         Imgcodecs.imwrite(outputFilenamePreamble + "_GRAY.png", grayROI);
-	    RobotLogCommon.d(TAG, "Writing " + outputFilenamePreamble + "_GRAY.png");
+        RobotLogCommon.d(TAG, "Writing " + outputFilenamePreamble + "_GRAY.png");
 
-	    Mat adjustedGray = imageUtils.adjustGrayscaleBrightness(grayROI, pBarcodeParameters.grayParameters.target);
-	    Imgcodecs.imwrite(outputFilenamePreamble + "_ADJ.png", adjustedGray);
-	    RobotLogCommon.d(TAG, "Writing adjusted grayscale image " + outputFilenamePreamble + "_ADJ.png");
+        Mat adjustedGray = imageUtils.adjustGrayscaleBrightness(grayROI, pBarcodeParameters.grayParameters.target);
+        Imgcodecs.imwrite(outputFilenamePreamble + "_ADJ.png", adjustedGray);
+        RobotLogCommon.d(TAG, "Writing adjusted grayscale image " + outputFilenamePreamble + "_ADJ.png");
 
-	    //??TODO Do you need a sharpening kernel? First try without.
+        //??TODO Do you need a sharpening kernel? First try without.
         /*
 	    // 7/14/2021 Try an image sharpening kernel. See also pyimagesearch Lesson
 	    // 102 for a different kernel and the scratchpad file UnsharpMask.cpp for
@@ -116,72 +108,76 @@ public class BarcodeRecognition {
 
         int grayThresholdLow = pBarcodeParameters.grayParameters.low_threshold;
         RobotLogCommon.d(TAG, "Inverse threshold values: low " + grayThresholdLow + ", max 255 (white)");
-    
+
         Mat thresholded = new Mat(); // output binary image
         Imgproc.threshold(adjustedGray, thresholded,
-            grayThresholdLow,    // threshold value
-            255,   // value assigned to pixels *under* the threshold value
-            Imgproc.THRESH_BINARY_INV); // thresholding type
-    
+                grayThresholdLow,    // threshold value
+                255,   // value assigned to pixels *under* the threshold value
+                Imgproc.THRESH_BINARY_INV); // thresholding type
+
         Imgcodecs.imwrite(outputFilenamePreamble + "_ADJ_THR.png", thresholded);
         RobotLogCommon.d(TAG, "Writing " + outputFilenamePreamble + "_ADJ_THR.png");
 
         //??TODO erode/dilate to eliminate white noise?
-   
-	    // The next step is to reduce the thresholded image by column to a single row
-	    // of pixels that contains the maximum value for each column. Since our thresholded
-	    // image is binary there are only two values, 0 (black) and 255 (white), that figure
-        // into the reduction.
-	    Mat reduced = new Mat();
-	    Core.reduce(thresholded, reduced, 0, Core.REDUCE_MAX);
-	    Imgcodecs.imwrite(outputFilenamePreamble + "_STRIPE.png", reduced);
-	    RobotLogCommon.d(TAG, "Writing " + outputFilenamePreamble + "_STRIPE.png");
 
-	    // Locate the white (because inverted) pixels that belong to the black Team Shipping Element.
+        // The next step is to reduce the thresholded image by column to a single row
+        // of pixels that contains the maximum value for each column. Since our thresholded
+        // image is binary there are only two values, 0 (black) and 255 (white), that figure
+        // into the reduction.
+        Mat reduced = new Mat();
+        Core.reduce(thresholded, reduced, 0, Core.REDUCE_MAX);
+        Imgcodecs.imwrite(outputFilenamePreamble + "_STRIPE.png", reduced);
+        RobotLogCommon.d(TAG, "Writing " + outputFilenamePreamble + "_STRIPE.png");
+
+        // Locate the white (because inverted) pixels that belong to the black Team Shipping Element.
         // See https://stackoverflow.com/questions/18147611/opencv-java-how-to-access-coordinates-returned-by-findnonzero
         MatOfPoint nzLocations = new MatOfPoint();
         Core.findNonZero(reduced, nzLocations);
 
-	    //## Remember: white pixels now represent the Team Scoring Element.
+        //## Remember: white pixels now represent the Team Scoring Element.
         //**TODO use a const with better minimum number of white pixels.
-	    RobotLogCommon.d(TAG, "Number of white pixels " + nzLocations.size().width);
-	    if (nzLocations.size().width < 3) {
-		    RobotLogCommon.d(TAG, "The stripe contains < 3 white pixels.");
-		    return new BarcodeReturn(false, RobotConstantsFreightFrenzy.BarcodeElementWithinROI.BARCODE_ELEMENT_NPOS);
-	    }
+        RobotLogCommon.d(TAG, "Number of white pixels " + nzLocations.size().width);
+        if (nzLocations.size().width < 3) {
+            RobotLogCommon.d(TAG, "The stripe contains < 3 white pixels.");
+            return new BarcodeReturn(false, RobotConstantsFreightFrenzy.BarcodeElementWithinROI.BARCODE_ELEMENT_NPOS);
+        }
 
-	    // MatOfPoint.get returns double[] where [0] = x, [1] = y
-	    double firstWhitePixel = nzLocations.get(0, 0)[0];
-	    double lastWhitePixel = nzLocations.get(0, nzLocations.cols() - 1)[1];
-	    double whiteStripeCenter = firstWhitePixel + ((lastWhitePixel - firstWhitePixel) / 2);
-	    RobotLogCommon.d(TAG, "Found first white pixel at x position " + firstWhitePixel);
-	    RobotLogCommon.d(TAG, "Found last white pixel at x position " + lastWhitePixel);
-	    RobotLogCommon.d(TAG, "Length of white pixel stripe " + (lastWhitePixel - firstWhitePixel));
-	    RobotLogCommon.d(TAG, "Center of white stripe at x position " + whiteStripeCenter);
+        // MatOfPoint.get returns double[] where [0] = x, [1] = y
+        double firstWhitePixel = nzLocations.get(0, 0)[0];
+        double lastWhitePixel = nzLocations.get(0, nzLocations.cols() - 1)[1];
+        double whiteStripeCenter = firstWhitePixel + ((lastWhitePixel - firstWhitePixel) / 2);
+        RobotLogCommon.d(TAG, "Found first white pixel at x position " + firstWhitePixel);
+        RobotLogCommon.d(TAG, "Found last white pixel at x position " + lastWhitePixel);
+        RobotLogCommon.d(TAG, "Length of white pixel stripe " + (lastWhitePixel - firstWhitePixel));
+        RobotLogCommon.d(TAG, "Center of white stripe at x position " + whiteStripeCenter);
 
-	    // Draw a small circle in the original image at the center of the white stripe. The circle should
+        // Draw a small circle in the original image at the center of the white stripe. The circle should
         // appear n the Team Scoring Element.
-	    Mat markTeamScoringElement = imageROI.clone();
+        Mat markTeamScoringElement = imageROI.clone();
 
-	    //**TODO hardcode center of circle - dependent on stripeROI above
-	    //Imgproc.circle(markTeamScoringElement, new Point(whiteStripeCenter, 67), 10, new Scalar(0, 0, 255), 3, 8, 0);
-	    //Imgcodecs.imwrite(pOutputFilenamePreamble + "_CIR.png", markmarkTeamScoringElement);
-	    //RobotLogCommon.d(TAG, "Writing " + pOutputFilenamePreamble + "_CIR.png");
+        //**TODO hardcode center of circle - dependent on stripeROI above
+        //Imgproc.circle(markTeamScoringElement, new Point(whiteStripeCenter, 67), 10, new Scalar(0, 0, 255), 3, 8, 0);
+        //Imgcodecs.imwrite(pOutputFilenamePreamble + "_CIR.png", markmarkTeamScoringElement);
+        //RobotLogCommon.d(TAG, "Writing " + pOutputFilenamePreamble + "_CIR.png");
 
-	    // Calculate the barcode position.
+        // Calculate the barcode position.
         RobotConstantsFreightFrenzy.BarcodeElementWithinROI positionFound = RobotConstantsFreightFrenzy.BarcodeElementWithinROI.BARCODE_ELEMENT_NPOS;
 
-        //**TODO Is firstWhitePixel >= array element lower
-        // limit (x) && <= current element + width.
         // Get the left window within the ROI from the barcode parameters.
         Map<RobotConstantsFreightFrenzy.BarcodeElementWithinROI, BarcodeParameters.BarcodeElement> barcodeElements =
-         pBarcodeParameters.getBarcodeElements();
+                pBarcodeParameters.getBarcodeElements();
         BarcodeParameters.BarcodeElement leftBarcodeElement = barcodeElements.get(RobotConstantsFreightFrenzy.BarcodeElementWithinROI.LEFT_WITHIN_ROI);
+        if ((firstWhitePixel >= leftBarcodeElement.x) && (firstWhitePixel <= leftBarcodeElement.x + leftBarcodeElement.width))
+            return new BarcodeReturn(false, RobotConstantsFreightFrenzy.BarcodeElementWithinROI.LEFT_WITHIN_ROI);
 
-        //**TODO STOPPED HERE 9/23/21
+        // Get the right window within the ROI from the barcode parameters.
         BarcodeParameters.BarcodeElement rightBarcodeElement = barcodeElements.get(RobotConstantsFreightFrenzy.BarcodeElementWithinROI.RIGHT_WITHIN_ROI);
+        if ((firstWhitePixel >= rightBarcodeElement.x) && (firstWhitePixel <= rightBarcodeElement.x + rightBarcodeElement.width))
+            return new BarcodeReturn(false, RobotConstantsFreightFrenzy.BarcodeElementWithinROI.RIGHT_WITHIN_ROI);
 
-        return new BarcodeReturn(false, positionFound);
+        // Didn't find the Team Shipping Element on either of the barcode elements
+        // we looked at.
+        return new BarcodeReturn(false, RobotConstantsFreightFrenzy.BarcodeElementWithinROI.BARCODE_ELEMENT_NPOS);
     }
 
 }
